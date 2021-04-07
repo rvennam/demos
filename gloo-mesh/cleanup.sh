1908a71c336f57b0012019a4443fce7efcffbd4d
Ram Vennam <therealrameth@gmail.com>
2021-04-07 13:07:36 -0400

    Update cleanup.sh

--- gloo-mesh/cleanup.sh
+++ gloo-mesh/cleanup.sh
@@ -1,4 +1,6 @@
 kind delete cluster --name kind-mgmt-cluster
+kind delete cluster --name mgmt-cluster
 kubectl config delete-context kind-mgmt-cluster
 kind delete cluster --name kind-remote-cluster
+kind delete cluster --name remote-cluster
 kubectl config delete-context kind-remote-cluster
